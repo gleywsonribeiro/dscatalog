a9e200140f7f5a4758c296a4b41df0fcb063e6c7
Gleywson Ribeiro <gleywson@gmail.com>
2022-12-04 17:24:14 -0300

    find by id, exception handling

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -2,21 +2,31 @@ package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
-import com.devsuperior.dscatalog.repositories.CategoryRespoitory;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
     @Autowired
-    private CategoryRespoitory categoryRespoitory;
+    private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll() {
-        return categoryRespoitory.findAll().stream().map(category -> new CategoryDTO(category)).collect(Collectors.toList());
+        //return categoryRepository.findAll().stream().map(category -> new CategoryDTO(category)).collect(Collectors.toList());
+        return categoryRepository.findAll().stream().map(CategoryDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> optional = categoryRepository.findById(id);
+        Category category = optional.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(category);
     }
 }
